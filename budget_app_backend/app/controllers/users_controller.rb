3f34589a2a5edbdf112dd4dffe54d614c76169cd
Itisafeature <thebigdebugger@gmail.com>
2019-12-11 14:46:39 -0500

    added index and new actions to users controller

--- budget_app_backend/app/controllers/users_controller.rb
+++ budget_app_backend/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+
+    def index
+        @users = User.all
+        render json: @users, status: :ok 
+    end
+
+    def new
+        @user = User.new
+    end
 end
